d145e4e1024495b81e67c5aec263eafb9d542706
Vidushi Bansal <vidushibansal@Velotios-MacBook-Air.local>
2025-05-19 19:34:57 +0530

    Added modules

--- modules/lookup/variables.tf
+++ modules/lookup/variables.tf
@@ -1,6 +1,9 @@
 variable "resource" {}
 
-variable "extra_query_params" {}
+variable "extra_query_params" {
+  type = map(string)
+  default = {}
+}
 
 variable "context_query_params" {
   description = "All context variables from Spacelift to send as query params"
